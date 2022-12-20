6d2f2cafb0a6e186899f86f9d1cf5e0eba20f34d
Matías Herrera <matias7122@amail.com>
2022-12-20 15:32:40 -0300

    model.server added method

--- models/models.server.ts
+++ models/models.server.ts
@@ -3,10 +3,20 @@ import { Sequelize } from 'sequelize';
 
 export class Server{
 	constructor(
-		private PORT: number = process.env.PORT; 
-		private app: express.Application = express();
-		private sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { host: 'localhost', dialect: 'mysql' });
+		private PORT: any = process.env.PORT | 3000,
+		private app: express.Application = express(),
+		private sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { host: 'localhost', dialect: 'mysql' })
 	){}
+
+	connectDB(){
+		try{
+			this.sequelize.authenticate();
+		}
+		catch(err){
+			console.log('An error has ocurred when was try connect to database');
+			console.log(err);
+		}
+	}
 
 	manageDB(){}
 
