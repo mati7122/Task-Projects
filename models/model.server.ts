a333e7cc3f5ad37d726ff3a9ae43c2a1e876d5b6
Matías Herrera <matias7122@gmail.com>
2022-12-21 22:30:52 -0300

    deleted duplicate models.server.ts from models

--- models/model.server.ts
+++ models/model.server.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import { Sequelize } from 'sequelize';
+import connection from '../db/sequelize.instance.ts';
 
 export class Server{
 	constructor(
 		private PORT: number = process.env.PORT; 
 		private app: express.Application = express();
 		private sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { host: 'localhost', dialect: 'mysql' });
-	){}
+	){
+		this.connectionDB();
+	}
+
+	async connectionDB(){
+		try{
+			await connection.authenticate()
+			console.log('Connection established correctly');
+		}
+		catch(err){
+			console.log(err);
+		}
+	}
 
 	manageDB(){}
 
