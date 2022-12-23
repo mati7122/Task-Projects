9b1042e7f1009630c40818e7313f750828bc505c
Matías Herrera <matias7122@gmail.com>
2022-12-23 14:50:43 -0300

    sequelize.task.ts - added fields

--- sequelize-models/sequelize.task.ts
+++ sequelize-models/sequelize.task.ts
@@ -1,33 +1,55 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../db/sequelize.instance';
+import Project from './sequelize.project';
+import User from './sequelize.user';
 
 const Task = sequelize.define('Task', {
-	name:{
+	name: {
 		type: DataTypes.STRING,
 		unique: true,
 		allowNull: false
 	},
-	fastDescription:{
+	fastDescription: {
 		type: DataTypes.TEXT
 	},
-	text: {
+	explicativeText: {
 		type: DataTypes.TEXT
 	},
-	initDate:{
+	initDate: {
 		type: DataTypes.DATE
 	},
 	finishDate: {
 		type: DataTypes.DATE
 	},
-	state: {
-		type: DataTypes.BOOLEAN
+	state: { //Indica en que estado se encuentra la tarea { 'active': 1, 'deleted': 0 }
+		type: DataTypes.BOOLEAN,
+		defaultValue: true
 	},
 	relevance: {
 		type: DataTypes.ENUM,
 		validate: {
-			isIn: [[ 'less importante', 'medium importance', 'very important' ]]
+			isIn: [['less importante', 'medium importance', 'very important']]
+		}
+	},
+	belongsToProject: {
+		type: DataTypes.NUMBER,
+		defaultValue: 'single task',
+		references: {
+			model: Project,
+			key: 'id'
+		}
+	},
+	createdBy: {
+		type: DataTypes.STRING,
+		defaultValue: 'anonymous user',
+		references: {
+			model: User,
+			key: 'id'
 		}
 	}
 }, { timestamps: false });
+
+// Task.belongsTo() //user
+// Task.belongsTo() //project
 
 export default Task;
