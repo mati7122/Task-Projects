a76c407ef8c1dbadc4f69b699f9ec27b6ab6ba71
Matías Herrera <matias7122@gmail.com>
2022-12-23 14:47:50 -0300

    model.task.controller.ts methods modified

--- models/model.task.controller.ts
+++ models/model.task.controller.ts
@@ -1,48 +1,54 @@
-import sequelize from "sequelize";
 import taskSequelizeInstance from "../sequelize-models/sequelize.task";
 
-interface TaskInterface{
+// interface TaskControllerInterface{
+// 	taskTable: Model<any, any>,
+
+// }
+
+interface TaskInterface {
 	taskName: string,
 	fastDescription: string,
 	completeDescription: string,
 	initDate: Date,
 	finishDate: Date,
-	state: boolean,
 	relevanceLabel: string
-}
+};
 
-export class taskController{
+export class TaskController {
 
-	private taskTable: sequelize.Model;
+	private taskTable;
 
-	constructor(){
-		this.taskTable = new taskSequelizeInstance()
+	constructor() {
+		this.taskTable = new taskSequelizeInstance();
 	}
 
-	async createTask({ taskName, fastDescription, completeDescription, initDate, finishDate, state, relevanceLabel }: TaskInterface){
-		const task = await this.taskTable.create({ taskName, fastDescription, completeDescription, initDate, finishDate, state, relevanceLabel });
+	async createTask({ taskName, fastDescription, completeDescription, initDate, finishDate, relevanceLabel }: TaskInterface) {
+		const task = await this.taskTable.create({ taskName, fastDescription, completeDescription, initDate, finishDate, relevanceLabel });
 		return task;
 	}
 
-	async getAllTask(){
+	async getAllTask() {
 		const allTask = await this.taskTable.findAll();
 		return allTask;
 	}
 
-	async getTaskById(id: number){
-		const task = await this.taskTable.findById(id);
+	async getTaskById(id: number) {
+		const task = await this.taskTable.findByPK(id);
 		return task;
 	}
 
-	async updateTask(id: number, ...rest: TaskInterface[]){
+	async updateTask(id: number, ...rest) {
 		const task = await this.taskTable.update({ rest }, {
 			where: { id }
 		});
 		return task;
 	}
 
-	async deleteTask(id: number){
-		const task = await this.taskTable.destroy({ where: { id } });
+	async deleteTask(id: number) {
+		// const task = await this.taskTable.destroy({ where: { id } });
+		const task = await this.taskTable.update({ state: false }, {
+			where: { id }
+		})
 		return task;
 	}
 
