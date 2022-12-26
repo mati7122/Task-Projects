60091a5128142a1e6d64c1181da58d36ca5853d6
Matías Herrera <matias7122@gmail.com>
2022-12-26 16:20:04 -0300

    Apply refactor to the file, moving the routes controller to other files

--- routes/routes.project.ts
+++ routes/routes.project.ts
@@ -1,120 +1,17 @@
 import { Router, Request, Response } from 'express';
-import { ProjectController } from "../models/model.project.controller";
+import { MessageSuccess, MessageError } from '../helpers/helper.messages';
+import { createController, getAllController, getByIdController, updateController, deleteController } from '../controller/controller.project';
 
-const projectControllerInstance = new ProjectController(); 
 const router = Router();
 
-type ErroMessage = {
-    message: string,
-    err: string
-}
+router.post('/create', createController);
 
-const MessageError = (err) => {
-    return {
-        message: 'An error has ocurred',
-        err
-    }
-};
+router.get('/get-all', getAllController);
 
-const MessageSuccess = (data) => {
-    return {
-        message: 'Request success!',
-        data
-    }
-};
+router.get('/get/:id', getByIdController);
 
-router.post('/create', async (req: Request, res: Response) => {
+router.put('/update/:id', updateController);
 
-    const { body } = req;
-    
-    try{
-
-        const data = await projectControllerInstance.createProject(body);
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.get('/get-all', async (req: Request, res: Response) => {
-
-    try{
-
-        const data = await projectControllerInstance.getAllProject();
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.get('/get/:id', async (req: Request, res: Response) => {
-
-    const { id } = req.params;
-
-    try{
-
-        const data = await projectControllerInstance.getProjectById(Number(id));
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.put('/update/:id', async (req: Request, res: Response) => {
-
-    const { id } = req.params;
-    const { body } = req;
-
-    try{
-
-        const data = await projectControllerInstance.updateProject(Number(id), body);
-
-        res.status(200).json( MessageSuccess(data) )
-
-    }
-    catch(err){
-        
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.delete('/delete/:id', async (req: Request, res: Response) => { //TODO: No funciona como quiero, "elimina" cualquier registro sin importar su estado
-
-    const { id } = req.params;
-
-    try{
-
-        const data = await projectControllerInstance.deleteProject(Number(id));
-
-        res.status(200).json( MessageSuccess(data.projectUpdated) );
-
-    }
-    catch(err){
-        
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
+router.delete('/delete/:id', deleteController);
 
 export default router;
--- routes/routes.task.ts
+++ routes/routes.task.ts
@@ -1,120 +1,17 @@
 import { Router, Request, Response } from 'express';
-import { TaskController } from "../models/model.task.controller";
+import { MessageSuccess, MessageError } from '../helpers/helper.messages';
+import { createController, getAllController, getByIdController, updateController, deleteController } from '../controller/controller.task';
 
-const taskControllerInstance = new TaskController(); 
 const router = Router();
 
-type ErroMessage = {
-    message: string,
-    err: string
-}
+router.post('/create', createController);
 
-const MessageError = (err) => {
-    return {
-        message: 'An error has ocurred',
-        err
-    }
-};
+router.get('/get-all', getAllController);
 
-const MessageSuccess = (data) => {
-    return {
-        message: 'Request success!',
-        data
-    }
-};
+router.get('/get/:id', getByIdController);
 
-router.post('/create', async (req: Request, res: Response) => {
+router.put('/update/:id', updateController);
 
-    const { body } = req;
-    
-    try{
-
-        const data = await taskControllerInstance.createTask(body);
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.get('/get-all', async (req: Request, res: Response) => {
-
-    try{
-
-        const data = await taskControllerInstance.getAllTask();
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.get('/get/:id', async (req: Request, res: Response) => {
-
-    const { id } = req.params;
-
-    try{
-
-        const data = await taskControllerInstance.getTaskById(Number(id));
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.put('/update/:id', async (req: Request, res: Response) => {
-
-    const { id } = req.params;
-    const { body } = req;
-
-    try{
-
-        const data = await taskControllerInstance.updateTask(Number(id), body);
-
-        res.status(200).json( MessageSuccess(data) )
-
-    }
-    catch(err){
-        
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.delete('/delete/:id', async (req: Request, res: Response) => { //TODO: No funciona como quiero, "elimina" cualquier registro sin importar su estado
-
-    const { id } = req.params;
-
-    try{
-
-        const data = await taskControllerInstance.deleteTask(Number(id));
-
-        res.status(200).json( MessageSuccess(data.taskUpdated) );
-
-    }
-    catch(err){
-        
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
+router.delete('/delete/:id', deleteController);
 
 export default router;
