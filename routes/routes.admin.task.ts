ec46f3df1dccf9e2d6fa33bebeab33aca08ba483
Matías Herrera <matias7122@gmail.com>
2022-12-28 02:15:39 -0300

    Deleted few imports, move controllers to another file
    Deleted few imports, move controllers to another file
    
    Removed few imports, moved controller to another file

--- routes/routes.admin.task.ts
+++ routes/routes.admin.task.ts
@@ -1,63 +1,14 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { TaskAdministrator } from "../classes/class.admin.task";
-import { MessageSuccess, MessageError } from '../helpers/helper.messages';
+import { getAllControllerAdmin, restoreControllerAdmin, deleteControllerAdmin } from '../controller/controller.admin.task';
 
 const taskControllerInstance = new TaskAdministrator();
 const router = Router();
 
-router.get('/get-all', async (req: Request, res: Response) => {
+router.get('/get-all', getAllControllerAdmin);
 
-    try{
+router.put('/restore/:id', restoreControllerAdmin);
 
-        const data = await taskControllerInstance.getAllTaskAdmin();
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.put('/restore/:id', async (req: Request, res: Response) => {
-
-    const { id } = req.params;
-
-    try{
-
-        const data = await taskControllerInstance.restoreTaskAdmin(Number(id));
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
-
-router.delete('/delete/:id', async (req: Request, res: Response) => { // !! This route delete task like "delete-hard"
-
-    const { id } = req.params;
-
-    try{
-
-        const data = await taskControllerInstance.deleteTaskAdmin(Number(id));
-
-        res.status(200).json( MessageSuccess(data) );
-
-    }
-    catch(err){
-
-        res.status(500).json( MessageError(err) );
-
-    }
-
-});
+router.delete('/delete/:id', deleteControllerAdmin);
 
 export default router;
