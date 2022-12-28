e1a8175b8447e6d3c79a358f6b4ee650b4aa5b56
Matías Herrera <matias7122@gmail.com>
2022-12-27 21:15:40 -0300

    Added new imports from controller.user; created new routes to getAll users and login user

--- routes/routes.user.ts
+++ routes/routes.user.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express';
-import { createController } from '../controller/controller.user';
+import { createController, getAllController, logInController } from '../controller/controller.user';
 
 const router = Router();
 
+router.get('/get-all', getAllController);
 router.post('/signin', createController);
-// router.post('/login', logInController);
+router.post('/login', logInController);
+
+export default router;
+
 // router.post('/forget', forgetPasswordOrUserNameController);
 
