56b52c500e153f2f778bac186a996ce0c727bf41
Matías Herrera <matias7122@gmail.com>
2022-12-28 15:29:16 -0300

    Added validation provided by express-validator in routes create, get-all, get, update and delete

--- routes/routes.task.ts
+++ routes/routes.task.ts
@@ -1,16 +1,28 @@
 import { Router } from 'express';
+import { body, param } from 'express-validator';
 import { createController, getAllController, getByIdController, updateController, deleteController } from '../controller/controller.task';
 
 const router = Router();
 
-router.post('/create', createController);
+router.post('/create',
+    body('name').exists().notEmpty().isString(),
+    body('fastDescription').exists().notEmpty().isString(),
+    body('explicativeText').exists().notEmpty().isString(),
+    body('initDate').exists().notEmpty().isString(),
+    body('finishDate').exists().notEmpty().isString(),
+    body('relevanceLabel').exists().notEmpty().isString(),
+    createController);
 
 router.get('/get-all', getAllController);
 
-router.get('/get/:id', getByIdController);
+router.get('/get/:id',
+    param('id').exists().notEmpty().isNumeric(),
+    getByIdController);
 
 router.put('/update/:id', updateController);
 
-router.delete('/delete/:id', deleteController);
+router.delete('/delete/:id',
+    param('id').exists().notEmpty().isNumeric(),
+    deleteController);
 
 export default router;
