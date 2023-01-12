da025fdaa97482fdd12927698f7bd20b90ffb8b8
Matías Herrera <matias7122@gmail.com>
2023-01-12 01:09:37 -0300

    Added new route /docs

--- routes/route.ts
+++ routes/route.ts
@@ -1,11 +1,16 @@
 import { Router, Request, Response } from 'express';
+import path from 'path';
 
 const router = Router();
 
+const relativePath = path.join(__dirname + '/../public/');
+
 router.get('/', (req: Request, res: Response) => {
-    res.render('home', {
-        name: 'My API REST'
-    });
+    res.sendFile(relativePath + 'index.html')
+});
+
+router.get('/docs', (req: Request, res: Response) => {
+    res.sendFile(relativePath + 'doc.html');
 });
 
 export default router;
