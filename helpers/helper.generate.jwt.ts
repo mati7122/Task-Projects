72e5b9683169d945099e51ca3178c6edeeaafafd
Matías Herrera <matias7122@gmail.com>
2023-01-12 01:28:13 -0300

    Deleted const token

--- helpers/helper.generate.jwt.ts
+++ helpers/helper.generate.jwt.ts
@@ -7,14 +7,14 @@ const env = load({
 
 export const generateJWT = (payload: Object) => new Promise((resolve, reject) => {
 
-    const token = jwt.sign(payload, env.JWT_PRIVATEKEY, (err, token) => {
+    jwt.sign(payload, env.JWT_PRIVATEKEY, (err, token) => {
 
-        if(err){
+        if (err) {
             console.log('An error has ocurred, we do can`t provide the token');
-            reject( err );
+            reject(err);
         }
 
-        resolve( token );
+        resolve(token);
 
     });
 
