86b47b4eef6168e4ebf9ad9a202f3571c4eebf4f
Matías Herrera <matias7122@gmail.com>
2023-01-18 02:57:33 -0300

    Added info comments about the routes in the module

--- routes/routes.admin.project.ts
+++ routes/routes.admin.project.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { getAllControllerAdmin, restoreControllerAdmin, deleteControllerAdmin } from '../controller/controller.admin.task';
 
+// ? This routes only can management by a user with an admin role
+
 const router = Router();
 
 router.get('/get-all', getAllControllerAdmin);
 
 router.put('/restore/:id', restoreControllerAdmin);
 
+// ! The next route realize a hard delete in your database, don`t use paranoid from sequelize
 router.delete('/delete/:id', deleteControllerAdmin);
 
 export default router;
--- routes/routes.admin.task.ts
+++ routes/routes.admin.task.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { param } from 'express-validator';
 import { getAllControllerAdmin, restoreControllerAdmin, deleteControllerAdmin } from '../controller/controller.admin.task';
 
+// ? This routes only can management by a user with an admin role
+
 const router = Router();
 
 router.get('/get-all', getAllControllerAdmin);
@@ -10,6 +12,7 @@ router.put('/restore/:id',
     param('id').isNumeric(),
     restoreControllerAdmin);
 
+// ! The next route realize a hard delete in your database, don`t use paranoid from sequelize
 router.delete('/delete/:id',
     param('id').isNumeric(),
     deleteControllerAdmin);
